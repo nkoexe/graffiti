8bece9ad8cdce2f28933a9817a811d51eb6f9288
nkoexe <niccolo.rag@gmail.com>
2025-06-15 19:54:49 +0200

    test bigger max file size

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: [],
+    },
+    api: {
+        bodyParser: {
+            sizeLimit: '20mb',
+        },
+    },
+    serverRuntimeConfig: {
+        maxFileSize: 20 * 1024 * 1024,
+    },
+
 };
 
 export default nextConfig;
